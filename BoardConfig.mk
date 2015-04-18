4b3977d79a75c3b0007f84c1f22054905e7e6031
Men_in_black007 <sanyam.53jain@gmail.com>
2015-04-18 04:35:02 -0400

    Tomato : Enable dex-preoptimization to speed up first boot sequence
    Change-Id: Ia698a90313dbde80e5a9fc8c301cabe91fe11261
    Signed-off-by: Men_in_black007 <sanyam.53jain@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -205,3 +205,12 @@ endif
 
 # inherit from the proprietary version
 -include vendor/yu/tomato/BoardConfigVendor.mk
+
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
