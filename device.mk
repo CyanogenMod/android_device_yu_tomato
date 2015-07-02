b686c71779e82dc4c32661fe130bddb1d1ec4f67
Pat Erley <perley@cyngn.com>
2015-07-02 16:13:20 -0700

    tomato: Enable DEXPREOPT and DEXPREOPT_BOOTIMG_ONLY for user builds
    Now that gms is being customized based on localization, we've gained
    enough space to enable some dex2oat packages. This improves first
    boot time and allows us to pass CTS.
    
    TOMATOL-531
    Conflicts:
    	device.mk
    
    Change-Id: I132b617c36cd1ea185d97ef3aedc190d0580134e

--- device.mk
+++ device.mk
@@ -280,3 +280,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_cfg.ini:system/etc/wifi/WCNSS_qcom_cfg.ini \
     $(LOCAL_PATH)/wifi/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
     $(LOCAL_PATH)/wifi/WCNSS_qcom_wlan_nv.bin:system/etc/firmware/wlan/prima/WCNSS_qcom_wlan_nv.bin
+
+ifeq ($(TARGET_BUILD_VARIANT),user)
+WITH_DEXPREOPT_BOOT_IMG_ONLY := true
+WITH_DEXPREOPT := true
+endif
+
