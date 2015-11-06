7d7dd210780ecfd4ee79fc52165af13b3aa7bf28
Meninblack007 <sanyam.53jain@gmail.com>
2015-11-06 06:57:28 -0500

    tomato: Fix compilation for yl_params
    Change-Id: Idb5611559d3b761857fd133bc5a71528e9e99755
    Signed-off-by: Meninblack007 <sanyam.53jain@gmail.com>

--- yl_params/yl_params.c
+++ yl_params/yl_params.c
@@ -19,10 +19,12 @@
 #define LOG_TAG "yl_params"
 
 #include <cutils/log.h>
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <string.h>
 #include <unistd.h>
 
 #include "yl_params.h"
