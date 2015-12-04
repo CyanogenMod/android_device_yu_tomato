b8616b5c038586d4924513c45649e3c77c4f83c2
Pat Erley <perley@cyngn.com>
2015-12-03 16:03:04 -0800

    tomato: Alphebatize init scripts
    Change-Id: I718856d47faac361f15d87ac415acccc72f6e4e2

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -2,18 +2,18 @@ LOCAL_PATH:= $(call my-dir)
 
 # Init scripts
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.ssr.rc
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.ssr.rc
-LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := fstab.qcom
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.ssr.rc
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.ssr.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
