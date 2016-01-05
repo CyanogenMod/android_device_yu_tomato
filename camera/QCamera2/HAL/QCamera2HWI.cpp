RonGokhale/android_hardware_qcom_camera

QCamera2: HAL: Fix for KW errors and banned APIs Fixes for KW errors and banned APIs in HAL.
Change-Id: Idbcb38e8e2a7f6ec73a81266de2596b351e97b32

QCamera2/HAL/QCamera2HWI.cpp
@@ -2390,10 +2390,10 @@ bool QCamera2HardwareInterface::processMTFDumps(qcamera_jpeg_evt_payload_t *evt)
         }
 
         if (allFocusImage)  {
-            strncpy(name, "AllFocusImage", CAM_FN_CNT - 1);
+            strlcpy(name, "AllFocusImage", CAM_FN_CNT);
             index = -1;
         } else {
-            strncpy(name, "0", CAM_FN_CNT - 1);
+            strlcpy(name, "0", CAM_FN_CNT);
         }
         CAM_DUMP_TO_FILE("/data/misc/camera/multiTouchFocus", name, index, "jpg",
                 dataPtr, dataLen);

CyanogenMod/android_device_yu_tomato

Signed-off-by: HridayHS <hridaysharma42@gmail.com>

camera/QCamera2/HAL/QCamera2HWI.cpp
@@ -2412,10 +2412,10 @@ bool QCamera2HardwareInterface::processMTFDumps(qcamera_jpeg_evt_payload_t *evt)
        }
 
        if (allFocusImage)  {
-           strncpy(name, "AllFocusImage", CAM_FN_CNT - 1);
+           strncpy(name, "AllFocusImage", CAM_FN_CNT);
            index = -1;
        } else {
-           strncpy(name, "0", CAM_FN_CNT - 1);
+           strncpy(name, "0", CAM_FN_CNT);
        }
        CAM_DUMP_TO_FILE("/data/misc/camera/multiTouchFocus", name, index, "jpg",
                dataPtr, dataLen);