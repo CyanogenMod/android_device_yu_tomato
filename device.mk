2a5c3d9c45a4ddff58fc8f90300a93518f32b73a
Zhao Wei Liew <zhaoweiliew@gmail.com>
2016-05-03 17:40:44 +0800

    tomato: Remove yl_btmac
    We no longer need this.
    
    Change-Id: I7aa8cc1a78c3aca919d1a6e063cae6515ffa4048

--- device.mk
+++ device.mk
@@ -54,10 +54,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
     $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
-# Bluetooth
-PRODUCT_PACKAGES += \
-    yl_btmac
-
 # Camera
 PRODUCT_PACKAGES += \
     camera.msm8916 \
