2c8fb13d6ea66a4ff9dfce544c411e10c6817fa9
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-05-07 23:16:56 +0300

    Revert "tomato: Specify TARGET_LOCALES"
    * CyanogenMod builds have enough space for all languages
    
    This reverts commit fc5b9898034d9c89d53b156e53c760db55384287.
    
    Change-Id: I4eb6d4d7b17c74a6261269526e26efdf39d85864

--- full_tomato.mk
+++ full_tomato.mk
@@ -16,9 +16,6 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-
-TARGET_LOCALES := en_US en_IN en_GB hi_IN mr_IN ml_IN ta_IN kn_IN te_IN
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from tomato device
