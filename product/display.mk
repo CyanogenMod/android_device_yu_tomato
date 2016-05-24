8084ee1ead0cb4f356559bab863541e8d30a5261
Ethan Chen <intervigil@gmail.com>
2016-05-25 03:06:00 +0800

    tomato: Shift to new device fragment model
     * Use make fragments for various subsystems
     * Get rid of dexopt
    
    Change-Id: I4fa13ab7cc66928e43d9274612bb3fd137c0094a

---
+++ product/display.mk
@@ -0,0 +1,9 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 720
+
+# Screen density
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+$(call inherit-product, frameworks/native/build/phone-xxhdpi-2048-hwui-memory.mk)
