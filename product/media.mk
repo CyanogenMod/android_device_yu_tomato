66be4dd8613aa08943927be4ca2eb43fd6709495
Ethan Chen <intervigil@gmail.com>
2016-05-25 03:06:00 +0800

    tomato: Shift to new device fragment model
     * Use make fragments for various subsystems
     * Get rid of dexopt
    
    Change-Id: I4fa13ab7cc66928e43d9274612bb3fd137c0094a

---
+++ product/media.mk
@@ -0,0 +1,3 @@
+# Media
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
---
+++ product/qcom-radio.mk
@@ -0,0 +1,16 @@
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.target=dpm1 \
+    persist.radio.multisim.config=dsds \
+    ro.config.always_show_roaming=true \
+    rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
+    ril.ecclist=000,08,100,101,102,110,112,118,119,120,122,911,999 \
+    ro.telephony.default_network=9,1
+
+# RIL
+ifeq ($(QCPATH),)
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/data/netmgr_config.xml:system/etc/data/netmgr_config.xml \
+    $(LOCAL_PATH)/configs/data/qmi_config.xml:system/etc/data/qmi_config.xml \
+    $(LOCAL_PATH)/configs/data/dsi_config.xml:system/etc/data/dsi_config.xml
+endif
