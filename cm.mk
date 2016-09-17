8276a3d34d264fcf0454e2c6bc71e6e6e5b58a84
Michael Bestas <mikeioannina@cyanogenmod.org>
2016-09-17 08:02:30 -0700

    Revert "Revert "Use common_mini_phone""
    * We don't have enough space in system
    
    This reverts commit 092151fadbf59ed80bc07bbdb2ab76c0ac29b177.
    
    Change-Id: Ic15c7fadc37bfbf46145931b4ec3c03176495f71

--- cm.mk
+++ cm.mk
@@ -15,7 +15,7 @@
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/cm/config/common_mini_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
