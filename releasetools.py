8b9ee8cdca5958e86076c92cff26b01df413f6b0
Ethan Chen <intervigil@gmail.com>
2016-12-24 06:26:20 +0100

    tomato: Add assertions on baseband version
    * Only flash on allowed baseband
    * Since YU5510A model release, there's a lot of confusion
      and people are trying to flash its firmware (by replacing it in ZIPs)
      on a regular AO5510/YU5510, which leads to dead devices.
    * YU Forums Official Warning : http://forums.yuplaygod.com/threads/44176/
    
    Change-Id: I16f781955d79b1178b2a5e9d299223a7e4a9310e
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

---
+++ releasetools.py
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2016 The Android Open Source Project*
+# Copyright (c) 2011, The Linux Foundation. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import hashlib
+import common
+import re
+
+def FullOTA_Assertions(info):
+  AddBasebandAssertion(info)
+  return
+
+def IncrementalOTA_Assertions(info):
+  AddBasebandAssertion(info)
+  return
+
+def AddBasebandAssertion(info):
+  android_info = info.input_zip.read("OTA/android-info.txt")
+  m = re.search(r'require\s+version-baseband\s*=\s*(\S+)', android_info)
+  if m:
+    versions = m.group(1).split('|')
+    if len(versions) and '*' not in versions:
+      cmd = 'assert(tomato.verify_baseband(' + ','.join(['"%s"' % baseband for baseband in versions]) + ') == "1");'
+      info.script.AppendExtra(cmd)
+  return
