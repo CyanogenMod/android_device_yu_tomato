91d33b36fd6cb9e91e6cfd4161712839badd0c40
Ethan Chen <intervigil@gmail.com>
2016-12-24 06:26:20 +0100

    tomato: Add assertions on baseband version
    * Only flash on allowed baseband
    * Since YU5010A model release, there's a lot of confusion
      and people are trying to flash its firmware (by replacing it in ZIPs)
      on a regular YU5010, which leads to dead devices.
    
    Change-Id: I16f781955d79b1178b2a5e9d299223a7e4a9310e
    Signed-off-by: Louis Popi <theh2o64@gmail.com>

--- recovery/Android.mk
+++ recovery/Android.mk
@@ -1,27 +1,25 @@
+
 #
-# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2016 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-# http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-include device/cyanogen/msm8916-common/BoardConfigCommon.mk
+LOCAL_PATH := $(call my-dir)
 
-include device/yu/tomato/board/*.mk
-
-# Assertions
-TARGET_BOARD_INFO_FILE := device/yu/tomato/board-info.txt
-
-# Releasetools
-TARGET_RELEASETOOLS_EXTENSIONS := device/yu/tomato/
-
-# Inherit from proprietary files
--include vendor/yu/tomato/BoardConfigVendor.mk
+include $(CLEAR_VARS)
+LOCAL_C_INCLUDES := bootable/recovery
+LOCAL_SRC_FILES := recovery_updater.cpp
+LOCAL_MODULE := librecovery_updater_tomato
+LOCAL_MODULE_TAGS := eng
+include $(BUILD_STATIC_LIBRARY)
